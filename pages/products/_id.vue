aeeb13c9172e9dbbe693e75664bb2ac9de445f10
adebayo2020 <ajibadeadebayoengr@gmail.com>
2020-06-28 22:18:26 +0100

    created product page and add breadcrumbs

---
+++ pages/products/_id.vue
@@ -0,0 +1,23 @@
+<template>
+  <main>
+    <!-- Breadcrumbs -->
+    <div class="a-spacing-top-medium"></div>
+    <div class="container-fluid">
+      <ul class="a-unordered-list a-horizontal a-size-small">
+        <li>
+          <span class="a-list-item">
+            <a href="#" class="a-link-normal a-color-tertiary">category</a>
+          </span>
+        </li>
+        <li>
+          <span class="a-list-item">></span>
+        </li>
+        <li>
+          <span class="a-list-item">
+            <a href="#" class="a-link-normal a-color-tertiary">Product Title</a>
+          </span>
+        </li>
+      </ul>
+    </div>
+  </main>
+</template>
