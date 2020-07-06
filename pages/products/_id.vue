a006f26ccecd7568268f3358838645580fd0f3b4
adebayo2020 <ajibadeadebayoengr@gmail.com>
2020-07-06 01:01:02 +0100

    added product page middle section grid

--- pages/products/_id.vue
+++ pages/products/_id.vue
@@ -23,6 +23,7 @@
     <div class="container-fluid">
       <div class="dp-container">
         <div class="row">
+          <!-- First 3 grids: Product Image and Autor's Section -->
           <div class="col-lg-3 col-md-3 col-sm-4">
             <div class="leftCol">
               <!-- Image -->
@@ -69,6 +70,137 @@
                           </span>
                         </a>
                       </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <!-- Description -->
+          <div class="col-lg-6 col-md-6 col-sm-8">
+            <div class="centerCol">
+              <!-- Product Title -->
+              <div class="titleDiv">
+                <h1 class="productTitle">
+                  <span class="largeTitle">Harry Potter</span>
+                  <span class="smallTitle">Paperback</span>
+                </h1>
+              </div>
+              <!-- Author's name -->
+              <div class="bylineinfo">
+                by
+                <a href="#" class="authorName">
+                  Walter Isaacson
+                  <i
+                    class="fas fa-chevron-down"
+                    style="font-size: 80px !important; color: #555 !important"
+                  ></i>
+                </a>
+                (Author)
+              </div>
+              <div class="reviewGroup"></div>
+              <hr style="margin-top: 10px" />
+              <!-- A tags Dummy data -->
+              <div class="mediaMatrix">
+                <div class="formats">
+                  <a href="#" class="link-expander">
+                    >
+                    <span class="tmmShowPrompt"
+                      >See all 18 formats and editions</span
+                    >
+                  </a>
+                  <ul>
+                    <!-- Kindle -->
+                    <li class="swatchElement" style="width: 117px">
+                      <span class="a-list-item">
+                        <span class="a-button-toggle">
+                          <span class="a-button-inner">
+                            <a href="#" class="a-button-text">
+                              <span>Kindle</span>
+                              <br />
+                              <span class="a-color-secondary">-</span>
+                            </a>
+                          </span>
+                        </span>
+                        <span class="tmm-olp-links"></span>
+                        <span class="tmm-olp-links">
+                          <a href="#" class="a-size-mini">
+                            <span class="kcpAppBox">
+                              <span class="a-declarative">
+                                Read with Our
+                                <span class="a-text-bold">Free App </span>
+                              </span>
+                            </span>
+                          </a>
+                        </span>
+                      </span>
+                    </li>
+                    <!-- Audible -->
+                    <li class="swatchElement" style="width: 117px">
+                      <span class="a-list-item">
+                        <span class="a-button-toggle">
+                          <span class="a-button-inner">
+                            <a href="#" class="a-button-text">
+                              <span>
+                                <img
+                                  src="/img/audibleLogo.png"
+                                  class="img-fluid"
+                                  style="width: 20px"
+                                />
+                                Audible</span
+                              >
+                              <br />
+                              <span class="a-color-secondary">-</span>
+                            </a>
+                          </span>
+                        </span>
+                        <span class="tmm-olp-links"></span>
+                        <span class="tmm-olp-links">
+                          <a href="#" class="a-size-mini">
+                            <span class="kcpAppBox">
+                              <span class="a-declarative">
+                                <span class="a-text-bold">Free App </span> with
+                                your audible trial
+                              </span>
+                            </span>
+                          </a>
+                        </span>
+                      </span>
+                    </li>
+                    <!-- Paperback -->
+                    <li class="swatchElement" style="width: 117px">
+                      <span class="a-list-item">
+                        <span class="a-button-toggle">
+                          <span class="a-button-inner">
+                            <a href="#" class="a-button-text">
+                              <span>Paperback</span>
+                              <br />
+                              <span class="a-color-secondary">-</span>
+                            </a>
+                          </span>
+                        </span>
+                      </span>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+              <!-- Description -->
+              <div class="bookDescription">
+                <div class="bookDescriptionInner">This book is awesome</div>
+              </div>
+
+              <!-- Product Specification -->
+              <div class="aboutEbooksFeature">
+                <hr />
+                <div class="row">
+                  <div class="col-sm-4 mb-1">
+                    <div class="a-declarative">
+                      Length:
+                      <span
+                        ><a href="#"
+                          >386 pages <i class="a-icon a-icon-popover"></i></a
+                      ></span>
                     </div>
                   </div>
                 </div>
