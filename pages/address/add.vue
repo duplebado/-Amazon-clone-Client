0e02465027da0c2dce15af21fb51029ea301459a
adebayo2020 <ajibadeadebayoengr@gmail.com>
2020-08-25 09:14:43 +0100

    created the list of addresses page

--- pages/address/add.vue
+++ pages/address/add.vue
@@ -196,7 +196,9 @@
                   <div class="a-spacing-top-large">
                     <span class="a-button-register">
                       <span class="a-button-inner">
-                        <span class="a-button-text">Add address</span>
+                        <span class="a-button-text" @click="onAddAddress"
+                          >Add address</span
+                        >
                       </span>
                     </span>
                   </div>
@@ -249,8 +251,7 @@ export default {
         let data = {
           country: this.country,
           fullName: this.fullName,
-          streetAddress1: this.streetAddress1,
-          streetAddress2: this.streetAddress2,
+          streetAddress: this.streetAddress1 + ", " + this.streetAddress2,
           city: this.city,
           state: this.state,
           zipCode: this.zipCode,
